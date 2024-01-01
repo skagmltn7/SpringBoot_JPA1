9e5490d6a1aa19b73d41e2d376a89dbe02b80ec4
skagmltn7 <skagmltn7@hanmail.net>
2024-01-02 00:04:34 +0900

    [feat]register, search member

---
+++ project/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@DiscriminatorColumn(name = "dtype")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+
+}
+
+
