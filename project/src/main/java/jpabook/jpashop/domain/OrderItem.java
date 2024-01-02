79ae2436adecc2c3cd2b4edd842b3eb7c38b28f6
skagmltn7 <skagmltn7@hanmail.net>
2024-01-03 00:43:32 +0900

    [feat] order

--- project/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ project/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -26,4 +30,26 @@ public class OrderItem {
 
     private int count;
 
+    // 생성 메소드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    // 비지니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /**
+     *
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * count;
+    }
 }
