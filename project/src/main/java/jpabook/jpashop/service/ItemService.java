53a035e69acd834d06ffe3497096e12cf968499c
skagmltn7 <skagmltn7@hanmail.net>
2024-01-03 22:16:56 +0900

    [feat] connect web

--- project/src/main/java/jpabook/jpashop/service/ItemService.java
+++ project/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -16,15 +16,22 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     @Transactional
-    public void saveItem(Item item){
+    public void saveItem(Item item) {
         itemRepository.save(item);
     }
 
-    public List<Item> findItems(){
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
-    public Item findOne(Long id){
+    public Item findOne(Long id) {
         return itemRepository.findOne(id);
+    }
+
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 }
